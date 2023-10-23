ae61e1b2b91f6c037353cdc2e9e760fc886abaf1
MelnykovSV <svmelnykov@gmail.com>
2023-10-23 15:41:16 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { App } from "./components";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./theme";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './components';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme';
 
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "./redux/store";
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/test-task-trial">
+        <BrowserRouter basename="/goit-test-task">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
