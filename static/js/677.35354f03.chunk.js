e20f6d8cef9c830ff56ea3729ce6d379ef71543d
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-10-23 16:44:04 +0000

    Deploying to gh-pages from @ MelnykovSV/goit-test-task@8f6cf86e64724787b10c0b6ef5277e31a83777bd 🚀

--- static/js/677.35354f03.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task_trial=self.webpackChunktest_task_trial||[]).push([[677],{3677:function(t,n,s){s.r(n),s.d(n,{default:function(){return l}});var a,e=s(168),r=s(6088).Z.main(a||(a=(0,e.Z)(["\n  padding-top: 30px;\n"]))),i=s(7689),u=s(4398),c=s(184),l=function(){return(0,c.jsxs)(r,{className:"container",children:[(0,c.jsx)(u.UI,{}),(0,c.jsx)(i.j3,{})]})}}}]);
-//# sourceMappingURL=677.35354f03.chunk.js.map
